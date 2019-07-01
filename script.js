a571c07bfcbf5fe77d55e0e16b2e2a624c2f8122
Teatoller <steven.otieno1@gmail.com>
2019-07-01 22:13:03 +0300

    (set variables):implement js variables
    - add card types
    - add welcome message
    - add cards issued
    
    [Finishes #002]

---
+++ script.js
@@ -0,0 +1,9 @@
+let card1 = "Ace of Spades",
+    card2 = "Ten of Herats";
+
+    console.log("Welcome to Blackjack!");
+
+    console.log("You are dealt : ");
+
+    console.log(" " + card1);
+    console.log(" " + card2);
